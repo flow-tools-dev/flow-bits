6323b22d5d4ce4b923afd5bcbdcc8e95a9c84d26
Adam Allison <allisonadam81@gmail.com>
2025-12-01 17:24:54 -0800

    fixed dedupearrayby

--- src/array/dedupeArrayBy.ts
+++ src/array/dedupeArrayBy.ts
@@ -1,7 +1,10 @@
 export const dedupeArrayBy =
   <K extends PropertyKey, T extends Record<K, any>>(prop: K) =>
-  (arr: T[] = []): T[] => [
-    ...arr
-      .reduce((m, el) => (m.has(el[prop]) ? m : m.set(el[prop], el)), new Map())
-      .values(),
-  ];
+  (arr: T[] = []): T[] => {
+    const seen = new Map<any, boolean>();
+    return arr.filter((el) => {
+      if (seen.has(el[prop])) return false;
+      seen.set(el[prop], true);
+      return true;
+    });
+  };
